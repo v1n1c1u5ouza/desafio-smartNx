9bb136334e691a4b274ffff60ea99cd2c30a0692
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2025-09-29 02:51:27 -0300

    chore(infra + deps): ajustes na config e inicializacao do app e adicionando supertest

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-import 'dotenv/config';  
-
-import app from './app.js';
+import app, { init } from './app.js';
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`API on http://localhost:${port}`));
+
+if (process.env.NODE_ENV !== 'test') {
+  init().then(() => {
+    app.listen(port, () => console.log(`API on http://localhost:${port}`));
+  });
+}
+
+export default app;
